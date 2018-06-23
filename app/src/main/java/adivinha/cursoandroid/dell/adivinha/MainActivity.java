ca77590bbd49669350b8e2b7e20f9e32321b48c9
juliossena <juliosouzasena@gmail.com>
2018-06-23 14:01:00 -0300

    jogo adivinha pronto

--- app/src/main/java/adivinha/cursoandroid/dell/adivinha/MainActivity.java
+++ app/src/main/java/adivinha/cursoandroid/dell/adivinha/MainActivity.java
@@ -2,12 +2,37 @@ package adivinha.cursoandroid.dell.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botao_jogar_id);
+        textoResultado = (TextView) findViewById(R.id.resultado_id);
+
+        //textoResultado.setText("Texto Alterado");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                int numeroAleatorio = r.nextInt(10);
+
+                textoResultado.setText("número escolhido: " + numeroAleatorio);
+            }
+        });
     }
 }
